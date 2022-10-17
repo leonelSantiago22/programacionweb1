cedba45578156a4bd42665420afc1c0858c6e82c
leonelsantiago22 <santiago.rosas.leonel@gmail.com>
2022-10-17 10:05:48 -0500

    uso de canvas

---
+++ practica2/java.js
@@ -0,0 +1,26 @@
+var canvas  = document.getElementById("miCanvas");
+var ctx = canvas.getContext("2d");
+//ctx es el pinceñ
+var x = canvas.width/2;
+var y = canvas.height/2;
+
+dibujodePlanoenejeX();
+dibujodePlanoenejeY();
+
+function dibujodePlanoenejeX()
+{
+    ctx.beginPath();
+    ctx.moveTo(x,0); //donde empiza la linea 
+    ctx.lineTo(x,canvas.height);//donde termina
+    ctx.stroke();
+
+    
+}
+
+function dibujodePlanoenejeY()
+{
+    ctx.beginPath();
+    ctx.moveTo(0,y); //donde empiza la linea 
+    ctx.lineTo(canvas.width,y);//donde termina
+    ctx.stroke();
+}
