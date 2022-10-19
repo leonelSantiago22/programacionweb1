8476626c2ae32e3d7751ca7b0e320e6a1388d1e4
leonelSantiago22 <santiago.rosas.leonel@gmail.com>
2022-10-19 00:10:49 -0500

    Dibujo de los cuadritos

--- practica4/canvaimage.js
+++ practica4/canvaimage.js
@@ -6,7 +6,28 @@ img.src = "mario.png";
 
 img.onload = function()
 {
-    ctx.drawImage(img,40,35,25,20,50,50,50,50);
+
+    ctx.drawImage(img,0,275,47,47,0,50,50,50);
+    ctx.drawImage(img,47,275,47,47,50,50,50,50);
+    ctx.drawImage(img,141,275,47,48,100,50,50,50);
+    ctx.drawImage(img,0,138,47,48,100,200,50,50);
+    ctx.drawImage(img,416,91,47,48,100,100,50,50);
+    ctx.drawImage(img,0,91,47,48,100,150,50,50);
+    ctx.drawImage(img,47,91,47,48,150,150,50,50);
+    ctx.drawImage(img,0,138,47,48,100,200,50,50);
+    ctx.drawImage(img,47,138,47,48,150,200,50,50);
+    ctx.drawImage(img,94,138,47,48,150,100,50,50);
+    ctx.drawImage(img,230,462,47,48,200,100,50,50);
+    //barril rojo
+    ctx.drawImage(img,462,230,47,48,0,150,50,50);
+    ctx.drawImage(img,509,230,47,48,50,150,50,50);
+    ctx.drawImage(img,462,277,47,48,0,200,50,50);
+    ctx.drawImage(img,508,277,47,48,50,200,50,50);
+
+    ctx.drawImage(img,276,415,47,48,200,150,50,50);
+    ctx.drawImage(img,323,415,47,48,250,200,50,50);
+    ctx.drawImage(img,323,416,47,48,250,150,50,50);
+    ctx.drawImage(img,276,462,47,48,200,200,50,50);
 }
     
     
